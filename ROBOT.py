01a5ab6f2f4a90d4d6fd8d413236fb2cc33ef2b5
Faizan Parabtani <faizan11911@gmail.com>
2022-07-28 22:13:50 +0530

    fixed bugs

--- ROBOT.py
+++ ROBOT.py
@@ -29,14 +29,6 @@ class Robot:
 		self.minobs_dist = 100
 		self.count_down = 5
 
-	def move_backward(self):
-		self.vr = - self.minspeed
-		self.vl = - self.minspeed/2
-
-	def move_forward(self):
-		self.vr = self.minspeed
-		self.vl = self.minspeed
-
 	def avoid_obstacles(self, point_cloud, dt):
 		closest_obstacle = None
 		dist = np.inf
@@ -54,9 +46,18 @@ class Robot:
 				self.count_down = 5
 				self.move_forward()
 
+	def move_backward(self):
+		self.vr = - self.minspeed
+		self.vl = - self.minspeed/2
+
+	def move_forward(self):
+		self.vr = self.minspeed
+		self.vl = self.minspeed
+
 	def kinematics(self, dt):
 		self.x += ((self.vl+self.vr)/2) * math.cos(self.heading) * dt
 		self.y -= ((self.vl+self.vr)/2) * math.sin(self.heading) * dt
+		self.heading += (self.vr - self.vl) / self.w * dt
 
 		if self.heading > 2*math.pi or self.heading < -2*math.pi:
 			self.heading = 0
@@ -80,7 +81,7 @@ class Graphics:
 
 		#Map
 		self.robot = pygame.image.load(robot_img_path)
-		self.map = pygame.image.load(map_img_path)
+		self.map_img = pygame.image.load(map_img_path)
 
 		#Dimensions
 		self.height, self.width = dimensions
@@ -97,7 +98,7 @@ class Graphics:
 
 	def draw_sensor_data(self, point_cloud):
 		for point in point_cloud:
-			pygame.draw_circle(self.map, self.red, point, 3, 0)
+			pygame.draw.circle(self.map, self.red, point, 3, 0)
 
 
 class Ultrasonic:
